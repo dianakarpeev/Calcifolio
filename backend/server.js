fce11db990f08aea7de5402f69c5363965600fa9
Carlos-LX <celx@loxi.ca>
2023-04-28 15:45:40 -0400

    added my video controller thingy

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const app = require('./app.js');
 const port = 1339;
 const model = require("./models/projectDeadlineModelMongoDb");
+const videoModel = require("./models/videoModel");
 const url = process.env.URL_PRE + process.env.MONGODB_PWD + process.env.URL_POST;
 const dbName = "eventDeadline_db";
 
@@ -9,8 +10,10 @@ const dbName = "eventDeadline_db";
  * Initialize the database connection
  * @constructor
  */
+//videoModel.initialize(dbName, false, url)
 model.initialize(dbName, false, url)
     .then(
+        
         app.listen(port) // Run the server
     );
 
