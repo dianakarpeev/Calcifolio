9f8e8b18b1ef96825dbb2f60ffb34aa202904220
Carlos-LX <celx@loxi.ca>
2023-05-03 11:45:58 -0400

    modified validateUtils

--- backend/models/validateUtils.js
+++ backend/models/validateUtils.js
@@ -1,5 +1,8 @@
 const validator = require("validator");
 const { InvalidInputError } = require("./InvalidInputError");
+const validator = require('validator');
+const { InvalidTitleError } = require('./InvalidTitleError');
+const {InvalidLengthError } = require('./InvalidLengthError');
 
 //default attributes that are expected from the validation
 var default_date_options = {
@@ -133,4 +136,42 @@ function nameIsValid(name) {
   return true;
 }
 
-module.exports = { isValid, isValidName, isValidDate, nameIsValid, urlIsValid, dateIsValid };
+
+
+//VIDEO VALIDATORS
+
+
+/**Check to see if title is defined and if length is a timestamp
+
+ * @param {string} title
+ * @param {string} length
+ * @returns true if length and title are valid. Throws InvalidLengthError or InvalidTitleError otherwise
+ */
+function isValid2(title,length) {
+    if (!title) {
+        throw  new InvalidTitleError("Title must be a valid string");
+}
+if (validator.isTime(length,{hourFormat:'hour24', mode:'withSeconds'})) {
+    return true;
+}
+
+throw new InvalidLengthError("Length must be a valid time.")
+}
+function validTitle(title) {
+    if (!title) {
+        throw  new InvalidTitleError("Title must be a valid string");
+}
+return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+module.exports = { isValid, isValidName, isValidDate, nameIsValid, urlIsValid, dateIsValid, validTitle, isValid2 };
