8436ec433812f5ef44267c726063bbda0448be97
Carlos-LX <celx@loxi.ca>
2023-05-04 16:43:16 -0400

    added VideoLayout

---
+++ frontend/src/layouts/VideoLayout.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Outlet} from "react-router-dom";
+import {Header} from '../components/Header';
+import {Footer} from '../components/Footer';
+import { CrudButtons } from "../components/CrudButtons";
+
+function VideoLayout() {
+   return <div>
+            <Header />
+            <CrudButtons/>
+            <Outlet />
+            <Footer />	
+	
+          </div>
+}  
+export {VideoLayout};
