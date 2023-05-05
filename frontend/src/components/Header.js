60afe99f965162f710e1421b72e9064c76a0e5cc
Carlos-LX <celx@loxi.ca>
2023-05-05 14:57:25 -0400

    Sidebar stuff added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,25 +5,7 @@ import { Link } from 'react-router-dom';
 function Header() {
   const { collapseSidebar } = useProSidebar();
   return (
-    <div id="orange" style={{ display: 'flex', height: '100%' }}>
-
-
-<Sidebar>
-  <Menu>
-    <MenuItem component={<NavButton to="/" label="Home" />}/>
-    <MenuItem component={<NavButton to="/deadlines" label="Deadlines" />}/> 
-    <MenuItem component={<NavButton to="/videos" label="Videos" />}/> 
-    <MenuItem component={<NavButton to="/posts" label="Posts" />}/> 
-    <MenuItem component={<NavButton to="/about" label="About us" />}/> 
-    <MenuItem component={<NavButton to="/contact" label="Contact" />}/>
-
-  </Menu>
-</Sidebar>
-
-<main>
-        <button onClick={() => collapseSidebar()}>Collapse</button>
-      </main>
-    </div>
+<></>
   );
 }
 export default Header;
--- frontend/src/layouts/MainLayout.js
+++ frontend/src/layouts/MainLayout.js
@@ -3,6 +3,12 @@ import {Outlet} from "react-router-dom";
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { ProSidebarProvider } from "react-pro-sidebar";
+import { Sidebar, Menu, MenuItem, useProSidebar } from 'react-pro-sidebar';
+import NavButton from "components/NavButton";
+import { Link } from "react-router-dom";
+import {DiAndroid} from "react-icons/di";
+import {VscAccount, VscPlay} from "react-icons/vsc";
+import { MdCalendarMonth , MdHome, MdBrush} from "react-icons/md";
 
 function MainLayout() {
   const footer={
@@ -15,19 +21,33 @@ function MainLayout() {
     fontWeight: "bold",
     backgroundColor: "white",
   }
+  const { toggleSidebar, collapseSidebar} = useProSidebar();
+   return   (
+    
+   <div style={{ display: 'flex', height: '100%', minHeight: '100vh'}}>
+   <Sidebar defaultCollapsed="true" collapsedWidth="4%" onMouseEnter={() =>collapseSidebar()} onMouseLeave={() =>collapseSidebar()}>
+     <Menu style = {{textAlign: "left"}}>
+     <MenuItem icon={<MdHome/>} component={<Link to="/" label="Home" />}>Home</MenuItem>
+    <MenuItem icon={<MdCalendarMonth/>} component={<Link to="/deadlines" label="Deadlines" />}>Deadlines</MenuItem>
+    <MenuItem icon={<VscPlay/>} component={<Link to="/videos" label="Videos" />}>Videos</MenuItem>
+    <MenuItem icon={<MdBrush/>}component={<Link to="/posts" label="Posts" />}>Art</MenuItem> 
+    <MenuItem icon={<VscAccount/>} component={<Link to="/about" label="About us" />}>About us</MenuItem>
+    
+    <MenuItem component={<Link to="/contact" label="Contact" />}>Contact</MenuItem>
+     </Menu>
+   </Sidebar>
+   <main style={{ padding: 10 }}> 
+   <Outlet></Outlet>
+   
+   </main>
+ </div>
+);
 
-   return <div>
-            <Header />
-            <br/>
-            <div>
-            <Outlet/>
-            </div>
-            <br/>
-            <br/>
-            <div style={footer}>
-            <Footer />	
-            </div>
+
+
+
+            
            
-          </div>
+      
 }   
 export default MainLayout;
