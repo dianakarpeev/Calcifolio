b8e67c20a622a0ef5e1c4989cc75e0c0c37451bf
Diana Karpeev <2059788@johnabbottcollege.net>
2023-05-19 12:02:13 -0400

    Added 'Can add user to DB' test

--- backend/tests/userAccountModelMongoDB.test.js
+++ backend/tests/userAccountModelMongoDB.test.js
@@ -2,6 +2,9 @@ const model = require("../models/userAccountModelMongoDB.js");
 const dbName = "dbTest";
 const { MongoMemoryServer } = require("mongodb-memory-server");
 const { InvalidInputError } = require("../models/InvalidInputError.js");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 require("dotenv").config();
 jest.setTimeout(5000);
 var mongod;
@@ -30,4 +33,15 @@ afterEach(async () => {
   await model.close();
 });
 
-
+test("Can add user to DB", async() => {
+    const testUser = { username: "test", password: "Password!1234"};
+    await model.createUser(testUser.username, testUser.password);
+    
+    const cursor = (await model.getCollection()).find({username: testUser.username});
+    const results = await cursor.toArray();
+    
+    expect(Array.isArray(results)).toBe(true);
+    expect(results.length).toBe(1);
+    expect(results[0].username.toLowerCase() == testUser.username.toLowerCase()).toBe(true);
+    expect(await bcrypt.compare(testUser.password, results[0].password)).toBe(true);
+});
