1176b681941cc6a6663204faeba2a293727d201b
Diana Karpeev <2059788@johnabbottcollege.net>
2023-05-19 09:55:17 -0400

    Added handleDeleteUser()

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -13,7 +13,7 @@ router.post("/signin", handleCreateUser);
  * Handles creation of new user in the database.
  * @param {object} req Expects body parameters username and password
  * @param {object} res User was created message (200 success)
- * 
+ *
  * throws {InvalidInputError} if username or password are in an incorrect format 400
  * throws {DatabaseError} if the problem is related to the database connectivity and its interactions. 500
  * throws Exception if contact with unknown errors that are unexpected instead of 'swallowing' other types. 500
@@ -49,42 +49,79 @@ async function handleCreateUser(req, res) {
 }
 
 /* ----------------------------- Find User (GET) ---------------------------- */
-router.get("/user", handleGetUser);
+router.get("/user/:username", handleGetUser);
 
 /**
  * Handles the request of getting an existing user in the database.
  * @param {object} req Expects username body paramater
  * @param {object} res Sends user information
- * 
+ *
  * throws {InvalidInputError} if the username is empty or incorrect. 400
  * throws {DatabaseError} if the problem is related to the database connectivity and its interactions. 500
  * throws Exception if contact with unknown errors that are unexpected instead of 'swallowing' other types. 500
  */
-async function handleGetUser(req, res){
-    let userMessage;
+async function handleGetUser(req, res) {
+  let userMessage;
 
-    try {
-        let username = req.body.username;
-        let result = await model.getSingleUser(username);
-        console.log(result);
-        res.status(200);
-        res.send(result);
-    } catch (err) {
-        console.log(err.message);
+  try {
+    let username = req.params.username;
+    let result = await model.getSingleUser(username);
+    console.log(result);
+    res.status(200);
+    res.send(result);
+  } catch (err) {
+    console.log(err.message);
 
-        if (err instanceof InvalidInputError) {
-            userMessage =
-                "The request cannot be fulfilled due to invalid input: \n" +
-                err.message;
-            res.status(400);
-        } else if (err instanceof DatabaseError) {
-            userMessage = "Internal error: " + err.message;
-            res.status(500);
-        } else {
-            res.status(500);
-            userMessage = "Unexpected Error: failed to get user " + username;
-        }
-
-        res.send({ errorMessage: userMessage });
+    if (err instanceof InvalidInputError) {
+      userMessage =
+        "The request cannot be fulfilled due to invalid input: \n" +
+        err.message;
+      res.status(400);
+    } else if (err instanceof DatabaseError) {
+      userMessage = "Internal error: " + err.message;
+      res.status(500);
+    } else {
+      res.status(500);
+      userMessage = "Unexpected Error: failed to get user " + username;
     }
+
+    res.send({ errorMessage: userMessage });
+  }
+}
+
+/* -------------------------- Update Deadline (PUT) ------------------------- */
+router.put("/deadlines", handleUpdateUser);
+
+/* -------------------------- Delete User (DELETE) -------------------------- */
+router.delete("/deadlines/:username", handleDeleteUser);
+
+async function handleDeleteUser(req, res) {
+  let userMessage;
+
+  try {
+    let username = req.params.username;
+    let password = req.body.password;
+    let result = await model.handleDeleteUser(username, password);
+
+    res.status(200);
+    res.send(result);
+  } catch (err) {
+    console.log(err.message);
+
+    if (err instanceof InvalidInputError) {
+      userMessage =
+        "The request cannot be fulfilled due to invalid input:" +
+        "\n" +
+        err.message;
+      res.status(400);
+    } else if (err instanceof DatabaseError) {
+      userMessage = "Internal error: " + err.message;
+      res.status(500);
+    } else {
+      res.status(500);
+      userMessage = "Unexpected Error: failed to delete single event";
+    }
+
+    res.send({ errorMessage: userMessage });
+  }
 }
